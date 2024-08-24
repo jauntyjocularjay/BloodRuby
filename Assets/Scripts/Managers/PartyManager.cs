d1f97f8f2ed28ac594df3748d656f478c307d0ba
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-08-24 01:55:18 -0700

    removed old scene

--- Assets/Scripts/Managers/PartyManager.cs
+++ Assets/Scripts/Managers/PartyManager.cs
@@ -12,63 +12,7 @@ public class PartyManager : MonoBehaviour
 
 
 
-// public class Party : List<PartySpot>
-// {
-//     public Party(List<BattlerData> partyMembers)
-//     {
-//         if(partyMembers.Count > 5){
-//             throw new PartyOverflow();
-//         }
-//         else
-//         {
-//             for(int i = 0; i < partyMembers.Count; i++)
-//             {
-//                 this.Add(partyMembers[i]);
-//             }
-//         }
-//     }
 
-//     public BattlerData GetLeader()
-//     {
-//         return (BattlerData) this[0];
-//     }
-
-//     public BattlerData GetMonster(int index)
-//     {
-//         if(index <= 0 || index >= this.Count)
-//         {
-//             throw new PartyIndexInvalid($"Non-leader Monsters are only located at indices between 1-{this.Count-1} inside this PlayerParty.");
-//         }
-//         else
-//         {
-//             return (BattlerData) this[index];
-//         }
-//     }
-
-//     public BattlerData GetBattler(int index)
-//     {
-//         if(index < 0 || index > this.Count)
-//         {
-//             throw new PartyIndexInvalid($"Monsters in this party are between 0 and {this.Count - 1}");
-//         }
-//         else
-//         {
-//             return this[index];
-//         }
-//     }
-
-//     public void AddPartyMember(BattlerData monster)
-//     {
-//         if(this.Count > 5)
-//         {
-//             throw new PartyOverflow();
-//         }
-//         else
-//         {
-//             this.Add(monster);
-//         }
-//     }
-// } 
 
 public enum BattleFormation
 {
