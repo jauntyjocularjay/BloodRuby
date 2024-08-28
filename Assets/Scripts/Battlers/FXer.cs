0814284db85783d0427fc2ee47d717f7a943ca81
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-08-28 04:38:51 -0700

    started FXer scriptable object

---
+++ Assets/Scripts/Battlers/FXer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/FXer")]
+public class FXer : ScriptableObject
+{
+    public Sprite sprite;
+
+
+}
